80c0d8c946c69f495d2cb8c1a10274655c636a83
ahmederaoui <ahmederaoui1@gmail.com>
2023-02-12 15:55:45 +0100

    Static injection

---
+++ src/presentation/StaticInjection.java
@@ -0,0 +1,15 @@
+package presentation;
+
+import dao.DaoImp;
+import dao.IDao;
+import metier.IMetier;
+import metier.MetierImp;
+
+public class StaticInjection {
+    public static void main(String[] args) {
+        IDao dao = new DaoImp();
+        MetierImp metier = new MetierImp();
+        metier.setDao(dao);
+        System.out.println("result = "+ metier.calculer());
+    }
+}
